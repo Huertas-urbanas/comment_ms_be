057f6bc40f250fb882d3cff18c8a703b09fde8ac
Brandon S <bssanchezr7@gmail.com>
2021-11-14 15:19:25 -0500

    Creación del modelo

---
+++ commentApp/models/comment.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    user = models.IntegerField(null=True)
+    post = models.IntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    image =  models.URLField(blank=True)
+    
+    def datepublished(self): return self.date.strftime('%d %b %Y' )
