f8db0af7633d57e677b5ff588d2bc24017a78714
damarishernandez <cortezhernandezdamaris@gmail.com>
2021-11-16 11:26:24 -0500

    correction fields

--- commentApp/models/comment.py
+++ commentApp/models/comment.py
@@ -3,9 +3,7 @@ from django.db import models
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField()
-    user = models.IntegerField(null=True)
-    post = models.IntegerField(null=True)
-    date = models.DateTimeField(auto_now_add=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
     image =  models.URLField(blank=True)
     
     def datepublished(self): return self.date.strftime('%d %b %Y' )
--- commentApp/serializers/commentSerializer.py
+++ commentApp/serializers/commentSerializer.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id','content','user', 'post', 'datepublished','image']
+        fields = ['id','content', 'datepublished','image']
 
